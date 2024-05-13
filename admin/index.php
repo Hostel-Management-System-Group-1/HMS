a665711a3d1817e449e94a30a85513d5f8458ebd
Hemanta Neupane <np034acs220153@heraldcollege.edu.np>
2024-05-13 10:50:41 +0545

    Changes to UI added

--- admin/index.php
+++ admin/index.php
@@ -3,89 +3,124 @@ session_start();
 include('includes/config.php');
 if(isset($_POST['login']))
 {
-$username=$_POST['username'];
-$password=$_POST['password'];
-$stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (userName=?|| email=?) and password=? ");
-				$stmt->bind_param('sss',$username,$username,$password);
-				$stmt->execute();
-				$stmt -> bind_result($username,$username,$password,$id);
-				$rs=$stmt->fetch();
-				$_SESSION['id']=$id;
-				$uip=$_SERVER['REMOTE_ADDR'];
-				$ldate=date('d/m/Y h:i:s', time());
-				if($rs)
-				{
-                //  $insert="INSERT into admin(adminid,ip)VALUES(?,?)";
-   // $stmtins = $mysqli->prepare($insert);
-   // $stmtins->bind_param('sH',$id,$uip);
-    //$res=$stmtins->execute();
-					header("location:admin-profile.php");
-				}
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (userName=?|| email=?) and password=? ");
+    $stmt->bind_param('sss',$username,$username,$password);
+    $stmt->execute();
+    $stmt -> bind_result($username,$username,$password,$id);
+    $rs=$stmt->fetch();
+    $_SESSION['id']=$id;
+    $uip=$_SERVER['REMOTE_ADDR'];
+    $ldate=date('d/m/Y h:i:s', time());
+    if($rs)
+    {
+        header("location:admin-profile.php");
+    }
+    else
+    {
+        echo "<script>alert('Invalid Username/Email or password');</script>";
+    }
+}
+?>
 
-				else
-				{
-					echo "<script>alert('Invalid Username/Email or password');</script>";
-				}
-			}
-				?>
+<!DOCTYPE html>
+<html lang="en">
 
-<!doctype html>
-<html lang="en" class="no-js">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #007bff; /* Blue */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
 
-	<title>Admin login</title>
+        .login-container {
+            background-color: #ffffff;
+            padding: 40px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+            max-width: 400px;
+            width: 100%;
+        }
 
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap-social.css">
-	<link rel="stylesheet" href="css/bootstrap-select.css">
-	<link rel="stylesheet" href="css/fileinput.min.css">
-	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-	<link rel="stylesheet" href="css/style.css">
-</head
+        .login-container h1 {
+            text-align: center;
+            color: #333333;
+            margin-bottom: 30px;
+        }
+
+        .login-container label {
+            font-weight: bold;
+            color: #555555;
+        }
+
+        .login-container input[type="text"],
+        .login-container input[type="password"] {
+            width: 100%;
+            padding: 15px;
+            margin-bottom: 20px;
+            border: 1px solid #dddddd;
+            border-radius: 5px;
+            box-sizing: border-box;
+            transition: border-color 0.3s ease-in-out;
+        }
+
+        .login-container input[type="text"]:focus,
+        .login-container input[type="password"]:focus {
+            outline: none;
+            border-color: #007bff;
+        }
+
+        .login-container input[type="submit"] {
+            background-color: #007bff;
+            color: #ffffff;
+            border: none;
+            padding: 15px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s ease-in-out;
+        }
+
+        .login-container input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+
+        .login-container .error-message {
+            color: #ff0000;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+
 <body>
+    <div class="login-container">
+        <h1>Admin Login</h1>
+        <form action="" method="post">
+            <label for="username">Username or Email</label>
+            <input type="text" id="username" name="username" placeholder="Username" required>
 
-	<div class="login-page bk-img" style="background-image: url(img/login-bg.jpg);">
-		<div class="form-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3">
-						<h1 class="text-center text-bold text-light mt-4x">Hostel Management System by G1</h1>
-						<div class="well row pt-2x pb-3x bk-light">
-							<div class="col-md-8 col-md-offset-2">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" required>
 
-								<form action="" class="mt" method="post">
-									<label for="" class="text-uppercase text-sm">Your Username or Email</label>
-									<input type="text" placeholder="Username" name="username" class="form-control mb">
-									<label for="" class="text-uppercase text-sm">Password</label>
-									<input type="password" placeholder="Password" name="password" class="form-control mb">
-
-
-									<input type="submit" name="login" class="btn btn-primary btn-block" value="login" >
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap-select.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-	<script src="js/dataTables.bootstrap.min.js"></script>
-	<script src="js/Chart.min.js"></script>
-	<script src="js/fileinput.js"></script>
-	<script src="js/chartData.js"></script>
-	<script src="js/main.js"></script>
+            <input type="submit" name="login" value="Login">
+        </form>
+        <?php
+        if (isset($error_message)) {
+            echo '<div class="error-message">' . $error_message . '</div>';
+        }
+        ?>
+    </div>
 </body>
 
-<style> .foot{text-align: center; border: 1px solid black;}</style>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
 			<li class="ts-account">
 				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
 				<ul>
+					<li><a href="my-profile.php">My Account</a></li>
 					<li><a href="logout.php">Logout</a></li>
 				</ul>
 			</li>
